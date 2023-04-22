7576426192c993155b6b30cdf1d4d05ed208966b
quasimontecarlo <info@crltt.com>
2023-04-22 09:36:50 +0930

    adding two more patches, patched the border, backing up before attempting barpadding

--- config.def.h
+++ config.def.h
@@ -10,13 +10,15 @@ static const unsigned int gappx     = 15;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 1;    /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const unsigned int systrayspacing = 6;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12" };
-static const char dmenufont[]       = "monospace:size=10";
+static const int horizpadbar	    = 2;	/* horizontal padding for statusbar */
+static const int vertpadbar	    = 4;	/* vertical padding for statusbar */
+static const char *fonts[]          = { "FiraCode-Regular:size=10" };
+static const char dmenufont[]       = "FiraCode-Retina:size=12";
 static const char col_gray1[]       = "#3b4252";
 static const char col_gray2[]       = "#4c566a";
 static const char col_gray3[]       = "#81a1c1";
